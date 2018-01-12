bc6f016cfa046d73775712682033e281ac3e81c6
Mark op 't Hoog <mark_hoog@outlook.com>
2018-01-12 12:34:32 +0100

    Replaced atjs with tribute js
    One of the upsides is that tributejs has no dependencies on jquery.

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -18,28 +18,11 @@
 </template>
 
 <script>
-    import 'jquery.caret';
-    import 'at.js';
-
     export default {
         data() {
             return {
                 body: ''
             };
-        },
-
-        mounted() {
-            $('#body').atwho({
-                at: "@",
-                delay: 750,
-                callbacks: {
-                    remoteFilter: function(query, callback) {
-                        $.getJSON("/api/users", {name: query}, function(usernames) {
-                            callback(usernames)
-                        });
-                    }
-                }
-            });
         },
 
         methods: {
--- resources/assets/js/components/Wysiwyg.vue
+++ resources/assets/js/components/Wysiwyg.vue
@@ -13,18 +13,58 @@
 
 <style lang="scss">
     @import '~trix/dist/trix.css';
+    @import '~tributejs/dist/tribute.css';
 </style>
 
 <script>
     import Trix from 'trix';
+    import Tribute from 'tributejs';
 
     export default {
         props: ['name', 'value', 'placeholder'],
 
+        data() {
+            return {
+                query: ''
+            }
+        },
+
         methods: {
             change({target}) {
                 this.$emit('input', target.value)
+            },
+            remoteSearch(text, callback) {
+                this.query = text;
+
+                axios.get(`/api/users?name=${text}`)
+                    .then(function({data}) {
+                        callback(data);
+                    }).catch(() => {
+                        callback([]);
+                })
             }
+        },
+
+        mounted() {
+            let $vm = this;
+            let el = $vm.$refs.trix;
+
+            let tribute = new Tribute({
+                lookup: 'value',
+                values: function(text, cb){
+                    $vm.remoteSearch(text, cb);
+                }
+            }).attach(el);
+
+            el.addEventListener('tribute-replaced', function (e) {
+                // set selected range
+                let range = el.editor.getSelectedRange();
+                el.editor.setSelectedRange([range[0] - $vm.query.length, range[1]]);
+                // // delete typed text and insert the matched item
+                el.editor.deleteInDirection("forward");
+                el.editor.insertString(e.detail.item.original.value);
+            });
+
         },
 
         watch: {
