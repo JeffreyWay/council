1104681b42979b0b17c82b36fd7de71592ce1e49
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-01-15 10:23:33 -0500

    Ensure a unique channel name
    This was causing some tests to sporadically fail.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -50,11 +50,8 @@ $factory->define(App\Thread::class, function ($faker) {
 });
 
 $factory->define(App\Channel::class, function ($faker) {
-    $name = $faker->word;
-
     return [
-        'name' => $name,
-        'slug' => $name,
+        'name' => $faker->unique()->word,
         'description' => $faker->sentence
     ];
 });
