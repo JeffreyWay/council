be703a2ebfe908c69201efabef4cb49b713478a6
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-01-15 10:19:59 -0500

    Change var names

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -49,12 +49,12 @@ class ReadThreadsTest extends TestCase
     {
         $this->signIn(create('App\User', ['name' => 'JohnDoe']));
 
-        $threadByJohn = create('App\Thread', ['user_id' => auth()->id()]);
-        $threadNotByJohn = create('App\Thread');
+        $johnsThread = create('App\Thread', ['user_id' => auth()->id()]);
+        $janesThread = create('App\Thread');
 
         $this->get('threads?by=JohnDoe')
-            ->assertSee($threadByJohn->title)
-            ->assertDontSee($threadNotByJohn->title);
+            ->assertSee($johnsThread->title)
+            ->assertDontSee($janesThread->title);
     }
 
     /** @test */
