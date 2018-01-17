2e7779a5cff96d60af5a10749aa3a60923eac83e
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-01-17 15:06:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -56,7 +56,8 @@ class ThreadsController extends Controller
      * @param  \App\Rules\Recaptcha $recaptcha
      * @return \Illuminate\Http\Response
      */
-    public function store(Recaptcha $recaptcha) {
+    public function store(Recaptcha $recaptcha)
+    {
         $foo = 'bar';
 
         request()->validate([
