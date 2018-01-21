0e72b03ad7365f797acbe5b60c55939e69a6e8cb
imanghafoori <imanghafoori1@gmail.com>
2018-01-21 17:42:28 +0330

    using widget in page

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -28,25 +28,7 @@
                     </div>
                 </div>
 
-                @if (count($trending))
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Trending Threads
-                        </div>
-
-                        <div class="panel-body">
-                            <ul class="list-group">
-                                @foreach ($trending as $thread)
-                                    <li class="list-group-item">
-                                        <a href="{{ url($thread->path) }}">
-                                            {{ $thread->title }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                @endif
+                @widget('TrendingThreads')
             </div>
         </div>
     </div>
