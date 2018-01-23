f7ee9af50a0413c39cca3b19e632c22fbb749606
Massimo Ghinassi <GhiMax@users.noreply.github.com>
2018-01-23 21:07:16 +0100

    Add link to thread profile creator (#120)

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,7 +19,7 @@
                         <div class="panel-body">
                             <p>
                                 This thread was published {{ $thread->created_at->diffForHumans() }} by
-                                <a href="#">{{ $thread->creator->name }}</a>, and currently
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, and currently
                                 has <span
                                         v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}
                                 .
