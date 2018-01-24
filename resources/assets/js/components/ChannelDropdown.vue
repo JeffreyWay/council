61fb08beae682a31aeee2d2e295b9fd41414afb6
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-01-24 15:56:37 +0100

    Add channel filter dropdown vue component (#109)
    * add channel filter dropdown vue component
    
    * refactor component

---
+++ resources/assets/js/components/ChannelDropdown.vue
@@ -0,0 +1,57 @@
+<template>
+    <li class="dropdown" :class="{'open': toggle}">
+        <a href="#" class="dropdown-toggle" @click.prevent="toggle = !toggle"
+           aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span></a>
+
+        <div class="dropdown-menu channel-dropdown">
+            <div class="input-wrapper">
+                <input type="text" class="form-control" v-model="filter" placeholder="Filter Channels..."/>
+            </div>
+            <ul class="list-group channel-list">
+                <li class="list-group-item" v-for="channel in filteredThreads">
+                    <a :href="`/threads/${channel.slug}`" v-text="channel.name"></a>
+                </li>
+            </ul>
+        </div>
+    </li>
+</template>
+
+<style lang="scss">
+    .channel-dropdown{
+        padding:0;
+    }
+    .input-wrapper{
+        padding:.5rem 1rem;
+    }
+
+    .channel-list{
+        max-height: 400px; overflow:auto;
+        margin-bottom:0;
+        .list-group-item{
+            border-radius:0;
+            border-left: none;
+            border-right: none;
+        }
+    }
+</style>
+
+<script>
+    export default {
+        props: ['channels'],
+
+        data() {
+            return {
+                toggle:false,
+                filter: ''
+            }
+        },
+
+        computed: {
+            filteredThreads() {
+                return this.channels.filter(channel => {
+                    return channel.name.toLowerCase().includes(this.filter.toLocaleLowerCase())
+                })
+            }
+        }
+    }
+</script>
