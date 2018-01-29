f53431a3c65923dba3fca225d1b8e2a508f2e5b3
Tom Harper <tom@sticktech.co.uk>
2018-01-29 23:32:59 +0000

    Issue #85: Fix undefined variable channel in channels admin form partial

--- app/Http/Controllers/Admin/ChannelsController.php
+++ app/Http/Controllers/Admin/ChannelsController.php
@@ -26,7 +26,7 @@ class ChannelsController extends Controller
      */
     public function create()
     {
-        return view('admin.channels.create');
+        return view('admin.channels.create', ['channel' => new Channel]);
     }
 
     /**
