690002a96c7847bfeca9d20e9567535186a75b84
Jon Sugar <jonathansugar@mac.com>
2018-01-30 11:39:25 +0000

    Remove test email address from config/council.php

--- config/council.php
+++ config/council.php
@@ -3,7 +3,6 @@
 return [
     'administrators' => [
         // Add the email addresses of users who should be administrators here.
-        'jon@example.com'
     ],
     'reputation' => [
         'thread_published' => 10,
