f31773697d8da1398ca01273d1e0e74050bf0926
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-01-31 18:47:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -124,11 +124,11 @@ class InstallCommand extends Command
     protected function migrateDatabaseWithFreshCredentials($credentials)
     {
         foreach ($credentials as $key => $value) {
-            $configKey = strtolower(str_replace("DB_", "", $key));
+            $configKey = strtolower(str_replace('DB_', '', $key));
 
-            if ($configKey === 'password' && $value == 'null' ) {
-                config(["database.connections.mysql.{$configKey}" => '']);             
-                
+            if ($configKey === 'password' && $value == 'null') {
+                config(["database.connections.mysql.{$configKey}" => '']);
+
                 continue;
             }
 
