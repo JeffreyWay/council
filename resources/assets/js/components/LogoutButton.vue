9ae609983c1781c8f81b3d7e5adeaaed8065f46d
Yan Paiha <y.paiha@gmail.com>
2018-01-31 19:52:29 +0100

    Vueify Logout Link (#130)
    ... because i think it is not necessary to use hidden forms when we use Vue

---
+++ resources/assets/js/components/LogoutButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <a :href="route" @click.prevent="onClick">
+        <slot/>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            route: {
+                type: String,
+                default: "/logout"
+            }
+        },
+
+        methods: {
+            onClick() {
+                axios.post(this.route).then(response => {
+                    window.location.href = response.request.responseURL
+                });
+            }
+        }
+    };
+</script>
