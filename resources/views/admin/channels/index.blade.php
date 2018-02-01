8fc1c7afc3d44a321de58941b6d9264b9dacafbc
Brian O'Sullivan <brian@tykus.ie>
2018-02-01 23:23:17 +0000

    Use withCount to optimise queries

--- resources/views/admin/channels/index.blade.php
+++ resources/views/admin/channels/index.blade.php
@@ -24,7 +24,7 @@
                     <td>{{$channel->name}}</td>
                     <td>{{$channel->slug}}</td>
                     <td>{{$channel->description}}</td>
-                    <td>{{$channel->threads()->count()}}</td>
+                    <td>{{$channel->threads_count}}</td>
                     <td>
                         <a href="{{ route('admin.channels.edit', ['channel' => $channel->slug]) }}" class="btn btn-default btn-xs">Edit</a>
                     </td>
