a5307eaaaf917e3e15448c8605b5cba80bf85db8
Chenggang Wang <chenggang.wang@designlogic.com.au>
2018-02-02 14:48:46 +1100

    fix error

--- app/Thread.php
+++ app/Thread.php
@@ -101,7 +101,7 @@ class Thread extends Model
      */
     public function channel()
     {
-        return $this->belongsTo(Channel::class);
+        return $this->belongsTo(Channel::class)->withoutGlobalScope('active');
     }
 
     /**
