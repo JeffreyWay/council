ca68198c882fe57db839dc8c1832d7f2fd1392e4
Jon Sugar <jonathansugar@mac.com>
2018-02-05 22:02:20 +0000

    Remove old unused method.

--- app/Http/Controllers/Admin/UserSuspensionController.php
+++ app/Http/Controllers/Admin/UserSuspensionController.php
@@ -7,21 +7,6 @@ use App\Http\Controllers\Controller;
 
 class UserSuspensionController extends Controller
 {
-    public function update()
-    {
-        $user = User::findOrFail(request()->id);
-        $user->toggleActive();
-
-        if (request()->expectsJson()) {
-            return response($user, 201);
-        }
-
-        $activeStatus = $user->active ? 'active' : 'suspended';
-
-        return redirect(route('admin.users.index'))
-            ->with('flash', "User $user->name is $activeStatus!");
-    }
-
     public function destroy(User $user)
     {
         $user->update(['active' => false]);
