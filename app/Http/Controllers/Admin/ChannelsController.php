5f0eb6f6a8ffb00b5607feb951ee5bf165300191
Brian O'Sullivan <brian.osullivan@live.com>
2018-02-05 19:21:34 +0000

    Use withCount to optimise queries (#143)

--- app/Http/Controllers/Admin/ChannelsController.php
+++ app/Http/Controllers/Admin/ChannelsController.php
@@ -15,7 +15,7 @@ class ChannelsController extends Controller
      */
     public function index()
     {
-        $channels = Channel::withoutGlobalScopes()->orderBy('name', 'asc')->with('threads')->get();
+        $channels = Channel::withoutGlobalScopes()->orderBy('name', 'asc')->withCount('threads')->get();
 
         return view('admin.channels.index', compact('channels'));
     }
