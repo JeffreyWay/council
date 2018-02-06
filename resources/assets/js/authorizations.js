11beeef06325b21293735298af2e89efe935d557
Rob Rogers <rob.rogers@me.com>
2018-02-06 18:27:49 -0500

    authorizations.js should compare int to an int
    In the own method, we use triple equals to compare the model.prop with the user.id. But the user.id is an int and model[prop] is sometimes (always?) a string.

--- resources/assets/js/authorizations.js
+++ resources/assets/js/authorizations.js
@@ -2,7 +2,7 @@ let user = window.App.user;
 
 module.exports = {
     owns (model, prop = 'user_id') {
-        return model[prop] === user.id;
+        return parseInt(model[prop]) === user.id;
     },
 
     isAdmin () {
