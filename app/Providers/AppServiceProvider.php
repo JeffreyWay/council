649ac50384039ba13e755a377bcbd64d99e1ca1b
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-02-10 10:58:34 -0500

    Apply fixes from StyleCI (#172)
    [ci skip] [skip ci]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Trending;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
