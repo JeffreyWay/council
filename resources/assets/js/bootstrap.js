74aca2bb0ba10dbe1fdb267c2b4c87a968ec1326
Darius Matulionis <darius@matulionis.lt>
2018-02-12 13:43:02 +0200

    update npm dependencies

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -4,7 +4,18 @@ window._ = require('lodash');
 import InstantSearch from 'vue-instantsearch';
 import VModal from 'vue-js-modal'
 
-window.$ = window.jQuery = require('jquery');
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap-sass');
+} catch (e) { }
+
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
@@ -40,10 +51,21 @@ Vue.prototype.signedIn = window.App.signedIn;
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.App.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.events = new Vue();
 
