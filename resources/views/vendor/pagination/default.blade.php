d1fd1a2bf9bcdc41706316a78d5f4b41a787ecdf
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-02-12 21:22:58 -0500

    wip

---
+++ resources/views/vendor/pagination/default.blade.php
@@ -0,0 +1,38 @@
+@if ($paginator->hasPages())
+    <div class="text-center">
+        <ul class="inline-flex w-48 list-reset border py-2 my-4 rounded">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="disabled flex-1 border-r"><span>&laquo;</span></li>
+            @else
+                <li class="flex-1 border-r"><a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="text-blue font-bold">&laquo;</a></li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="disabled flex-1 border-r"><span>{{ $element }}</span></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="active flex-1 font-bold border-r"><span>{{ $page }}</span></li>
+                        @else
+                            <li class="flex-1 font-bold border-r"><a href="{{ $url }}" class="text-blue">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="flex-1"><a href="{{ $paginator->nextPageUrl() }}" rel="next" class="text-blue font-bold">&raquo;</a></li>
+            @else
+                <li class="disabled flex-1"><span>&raquo;</span></li>
+            @endif
+    </ul>
+    </div>
+@endif
