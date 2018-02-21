a4ff8fbf3c1d7996ba25cab6d75e3cc28e2934ba
Jørgen Birkhaug <jorgen.birkhaug@uib.no>
2018-02-21 10:18:48 +0100

    Don't show the widget if there are no users online

--- resources/assets/js/components/UsersOnline.vue
+++ resources/assets/js/components/UsersOnline.vue
@@ -1,37 +1,39 @@
 <template>
-   <div class="widget">
+   <div v-if="users.length" class="widget">
             <h4 class="widget-heading">Who's online?</h4>
 
-            <ul class="list-reset" v-if="users.length">
+            <ul class="list-reset">
                 <li class="pb-3 text-sm" v-for="user in users">
-                    <a class="link text-blue" :href="'/profiles/' + user.name">@{{ user.name }}</a> 
+                    <a 
+                        class="link text-blue"
+                        :href="'/profiles/' + user.name"
+                        v-html="'@' + user.name"
+                    >
+                    </a> 
             </li>
         </ul>  
     </div>
 </template>
 
 <script>
-
-
 export default {
-
   data() {
-        return {
-            users: []
-        }
-    },
+    return {
+      users: []
+    };
+  },
 
   created() {
     if (typeof window.Echo !== 'undefined' && window.App.signedIn) {
       window.Echo.join('forum.online')
-        .here((users) => {
-            this.users = users;
+        .here(users => {
+          this.users = users;
         })
-        .joining((user) => {
-            this.users.push(user);
+        .joining(user => {
+          this.users.push(user);
         })
-        .leaving((user) => {
-            this.users.splice(this.users.indexOf(user), 1)
+        .leaving(user => {
+          this.users.splice(this.users.indexOf(user), 1);
         });
     }
   }
