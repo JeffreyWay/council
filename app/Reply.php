da706d06135cf0eac3a77cd23de738e43ad4c99b
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-03-02 09:24:42 -0500

    Append xp to array form

--- app/Reply.php
+++ app/Reply.php
@@ -28,7 +28,7 @@ class Reply extends Model
      *
      * @var array
      */
-    protected $appends = ['favoritesCount', 'isFavorited', 'isBest'];
+    protected $appends = ['favoritesCount', 'isFavorited', 'isBest', 'xp'];
 
     /**
      * Boot the reply instance.
@@ -147,7 +147,7 @@ class Reply extends Model
         return $this->isBest();
     }
 
-    public function getXP()
+    public function getXpAttribute()
     {
         $xp = $this->isBest() ? config('council.reputation.best_reply_awarded') : 0;
         $xp += config('council.reputation.reply_posted');
