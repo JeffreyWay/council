857fbb30321bcd1f161574f66ecee255a840d122
Jon Sugar <jonathansugar@mac.com>
2018-03-03 00:27:33 +0000

    Add back blue dots. (#208)

--- resources/assets/js/components/ActivityLayout.vue
+++ resources/assets/js/components/ActivityLayout.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="flex items-center">
+        <div class="title">&nbsp;</div>
         <div class="mt-0 mr-0 mb-10 w-full pl-6">
             <div class="rounded border border-grey-light">
                 <div class="px-6 py-4">
