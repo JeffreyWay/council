47a95ed82a89bfbd24ba231ee4891fdad562b63b
Jon Sugar <jonathansugar@mac.com>
2018-03-05 15:44:51 +0000

    Use 'XP' instead of 'reputation'. Use standard 'green-pill' styling for XP.

--- resources/assets/js/components/Leaderboard.vue
+++ resources/assets/js/components/Leaderboard.vue
@@ -3,9 +3,9 @@
         <div v-for="(user, key) in leaderboard" :key="user.id" class="my-10 p-4 rounded border border-grey-light flex justify-between">
             <div class="flex">
                 <img :src="user.avatar_path" :alt="user.username" :class="avatarClasses(user.avatar_path)" class="w-16 h-16 rounded-full mr-3">
-                <div class="flex flex-col text-grey">
-                    <a :href="userProfileLink(user.username)" class="mb-4 text-xl text-blue">{{ user.username }}</a>
-                    <span class="text-grey-darkest font-bold">{{ user.reputation }} <span class="text-grey">reputation</span> </span>
+                <div>
+                    <a :href="userProfileLink(user.username)" class="inline mb-4 text-xl text-blue">{{ user.username }}</a>
+                    <span class="inline px-2 bg-green rounded font-semibold text-white">{{ user.reputation }} XP</span>
                 </div>
             </div>
             <span class="text-5xl text-grey-light">{{ key+1 }}</span>
