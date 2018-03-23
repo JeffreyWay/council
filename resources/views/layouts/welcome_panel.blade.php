282c82bc453c641385f1a710b0ce95ef28879d4f
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-23 09:26:39 +0100

    Since this isn't a nav, so why should we use the nav element instead of a normal div?

--- resources/views/layouts/welcome_panel.blade.php
+++ resources/views/layouts/welcome_panel.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-blue-dark py-4">
+<div class="bg-blue-dark py-4">
     <div class="container mx-auto flex justify-between items-center text-blue-lightest pl-6">
         <div>
             <h1 class="font-normal text-2xl">
@@ -6,4 +6,4 @@
             </h1>
         </div>
     </div>
-</nav>
+</div>
